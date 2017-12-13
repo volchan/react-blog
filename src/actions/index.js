6aebdc87fc6d36c007f5389e80ecbd8f4d397623
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-12-13 16:39:37 +0100

    session 9/142 done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import Axios from "axios";
 
 export const FETCH_POSTS = "FETCH_POSTS";
+export const CREATE_POST = "CREATE_POST";
+
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=RUBYRULEZ123"
 
@@ -11,4 +13,13 @@ export const fetchPosts = () => {
     type: FETCH_POSTS,
     payload: request
   });
+}
+
+export const createPost = (values) => {
+  const request = Axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return {
+    type: CREATE_POST,
+    pauload: request
+  }
 }
--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,11 +1,106 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createPost } from "../actions";
 
 class PostNew extends Component {
-  render() {
+  errorDisplay({ meta: { touched, error} } = field) {
+    return `form-group ${touched && error ? "has-danger" : ""}`
+  }
+
+  renderField(field) {
+    const { meta: { touched, error} } = field;
+
     return (
-      <div>Post New</div>
+      <div className={`form-group ${touched && error ? "has-danger" : ""}`}>
+        <label>{field.label}</label>
+        <input
+          type="text"
+          className="form-control"
+          {...field.input}
+        />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
+
+  renderTextArea(field) {
+    const { meta: { touched, error} } = field;
+
+    return (
+      <div className={`form-group ${touched && error ? "has-danger" : ""}`}>
+        <label>{field.label}</label>
+        <textarea
+          type="text"
+          className="form-control"
+          {...field.input}
+        />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    this.props.createPost(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <div>
+         <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+           <Field
+             name="title"
+             label="Title"
+             component={this.renderField}
+           />
+           <Field
+             name="categories"
+             label="Categories"
+             component={this.renderField}
+           />
+           <Field
+             name="content"
+             label="Content"
+             component={this.renderTextArea}
+           />
+           <button type="submit" className="btn btn-primary">Submit</button>
+           <Link to="/" className="btn btn-danger">Cancel</Link>
+         </form>
+      </div>
     );
   }
 }
 
-export default PostNew;
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  } else if (values.title.length <= 5) {
+    errors.title = "Must be longer than 5 characters!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content please!";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: "PostsNewForm"
+})(
+  connect(null, { createPost })(PostNew)
+);
