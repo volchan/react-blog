bb4d5c14670129fdc851f7d7c6695a5aaa9771d7
stephane delatorre <s.dlt@hotmail.fr>
2017-12-15 22:59:13 +0100

    refacto

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
+
+import reducers from '../reducers';
+import PostIndex from "../components/post_index";
+import PostNew from "../components/post_new";
+import PostShow from "../components/post_show";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/posts/new" component={PostNew} />
+              <Route path="/posts/:id" component={PostShow} />
+              <Route path="/" component={PostIndex} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import promise from "redux-promise";
 
-import reducers from './reducers';
-import PostIndex from "./components/post_index";
-import PostNew from "./components/post_new";
-import PostShow from "./components/post_show";
+import App from "./components/app";
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/posts/new" component={PostNew} />
-          <Route path="/posts/:id" component={PostShow} />
-          <Route path="/" component={PostIndex} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App /> , document.querySelector('.container'));
