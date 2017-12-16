bfa0fa4f0576bb14d583bf9c96ea268fb2f02973
stephane delatorre <s.dlt@hotmail.fr>
2017-12-16 10:48:29 +0100

    more refacto

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,6 @@
 import Axios from "axios";
 
-export const FETCH_POSTS = "FETCH_POSTS";
-export const CREATE_POST = "CREATE_POST";
-export const FETCH_POST = "FETCH_POST";
-export const DELETE_POST = "DELETE_POST";
+import { FETCH_POST, FETCH_POSTS, CREATE_POST, DELETE_POST } from "./types";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=RUBYRULEZ123"
